ddf6acd702a1ded3484219066d6bcf284233a3ed
dmitryk-dk <dmitry.kozlov@brightlocal.com>
2020-11-17 11:55:44 +0200

    improve builder tests

--- builder/builder_test.go
+++ builder/builder_test.go
@@ -1,6 +1,9 @@
 package builder
 
 import (
+	"log"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/BrightLocal/FrontBuilder/builder/files"
@@ -8,41 +11,195 @@ import (
 )
 
 func TestPrepareApps(t *testing.T) {
-	b := Builder{
-		htmlExtension: ".html",
-		scripts: []string{
-			"app/index.js",
-			"app/lib.js",
-		},
-		typeScripts: []string{
-			"index.ts",
-		},
-		htmls: map[string]*files.HTML{
-			"index.html":     {},
-			"app/index.html": {},
-		},
-		jsApps: make(map[string]string),
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("error get working dir %s", err)
 	}
-	b.prepareApps()
-	assert.Equal(t, map[string]string{
-		"index.html":     "index.ts",
-		"app/index.html": "app/index.js",
-	}, b.jsApps)
+	var testCases = map[string]Builder{
+		"default_app": {
+			htmlExtension: ".html",
+			scripts: []sourcePath{
+				{
+					BaseDir: filepath.Join(path, "test-project", "default", "app"),
+					Path:    "/app2.js",
+				},
+			},
+			typeScripts: []sourcePath{
+				{
+					BaseDir: filepath.Join(path, "test-project", "default", "app"),
+					Path:    "/index.ts",
+				},
+				{
+					BaseDir: filepath.Join(path, "test-project", "default", "app"),
+					Path:    "/app1.ts",
+				},
+			},
+			htmls: map[string]*files.HTML{
+				"/app1.html":  {},
+				"/app2.html":  {},
+				"/index.html": {},
+			},
+			jsApps: make(map[string]sourcePath),
+		},
+		"large_app": {
+			htmlExtension: ".html",
+			scripts: []sourcePath{
+				{
+					BaseDir: filepath.Join(path, "test-project", "large", "scripts"),
+					Path:    "app2/app2.js",
+				},
+			},
+			typeScripts: []sourcePath{
+				{
+					BaseDir: filepath.Join(path, "test-project", "large", "scripts"),
+					Path:    "/index.ts",
+				},
+				{
+					BaseDir: filepath.Join(path, "test-project", "large", "scripts"),
+					Path:    "app1/app1.ts",
+				},
+			},
+			htmls: map[string]*files.HTML{
+				"app1/app1.html": {},
+				"app2/app2.html": {},
+				"/index.html":    {},
+			},
+			jsApps: make(map[string]sourcePath),
+		},
+		"minimal_app": {
+			htmlExtension: ".htm",
+			scripts:       []sourcePath{},
+			typeScripts:   []sourcePath{},
+			htmls: map[string]*files.HTML{
+				"source/index.htm": {},
+			},
+			jsApps: make(map[string]sourcePath),
+		},
+	}
+	var testResults = map[string]map[string]sourcePath{
+		"default_app": {
+			"/app1.html": sourcePath{
+				BaseDir: filepath.Join(path, "test-project", "default", "app"),
+				Path:    "/app1.ts",
+			},
+			"/app2.html": sourcePath{
+				BaseDir: filepath.Join(path, "test-project", "default", "app"),
+				Path:    "/app2.js",
+			},
+			"/index.html": sourcePath{
+				BaseDir: filepath.Join(path, "test-project", "default", "app"),
+				Path:    "/index.ts",
+			},
+		},
+		"large_app": {
+			"/index.html": sourcePath{
+				BaseDir: filepath.Join(path, "test-project", "large", "scripts"),
+				Path:    "/index.ts",
+			},
+			"app1/app1.html": sourcePath{
+				BaseDir: filepath.Join(path, "test-project", "large", "scripts"),
+				Path:    "app1/app1.ts",
+			},
+			"app2/app2.html": sourcePath{
+				BaseDir: filepath.Join(path, "test-project", "large", "scripts"),
+				Path:    "app2/app2.js",
+			},
+		},
+		"minimal_app": {},
+	}
+	for i, tt := range testCases {
+		t.Run(i, func(t *testing.T) {
+			tt.prepareApps()
+			assert.Equal(t, testResults[i], tt.jsApps)
+		})
+	}
 }
 
 func TestCollect(t *testing.T) {
-	sources := []string{
-		"../test-projects/large/scripts",
-		"../test-projects/large/templates",
+	var testCases = map[string][]string{
+		"default_app": []string{
+			"../test-projects/default/app",
+		},
+		"large_app": []string{
+			"../test-projects/large/scripts",
+			"../test-projects/large/templates",
+		},
+		"minimal_app": []string{
+			"../test-projects/minimal/source",
+		},
 	}
-	b := NewBuilder(sources, "", false)
-	if assert.NoError(t, b.collectFiles()) {
-		assert.Equal(t, []string{
-			"../test-projects/large/scripts/",
-			"../test-projects/large/templates/",
-		}, b.sources)
-		assert.Equal(t, []string{"app2/app2.js"}, b.scripts)
-		assert.Equal(t, []string{"app1/app1.ts", "index.ts"}, b.typeScripts)
-		assert.Equal(t, 3, len(b.htmls))
+	var testResults = map[string]struct {
+		sources     []string
+		scripts     []sourcePath
+		typeScripts []sourcePath
+		htmls       int
+	}{
+		"default_app": {
+			sources: []string{
+				"../test-projects/default/app",
+			},
+			scripts: []sourcePath{
+				{
+					BaseDir: "../test-projects/default/app",
+					Path:    "/app2.js",
+				},
+			},
+			typeScripts: []sourcePath{
+				{
+					BaseDir: "../test-projects/default/app",
+					Path:    "/app1.ts",
+				},
+				{
+					BaseDir: "../test-projects/default/app",
+					Path:    "/index.ts",
+				},
+			},
+			htmls: 3,
+		},
+		"large_app": {
+			sources: []string{
+				"../test-projects/large/scripts",
+				"../test-projects/large/templates",
+			},
+			scripts: []sourcePath{
+				{
+					BaseDir: "../test-projects/large/scripts",
+					Path:    "/app2/app2.js",
+				},
+			},
+			typeScripts: []sourcePath{
+				{
+					BaseDir: "../test-projects/large/scripts",
+					Path:    "/app1/app1.ts",
+				},
+				{
+					BaseDir: "../test-projects/large/scripts",
+					Path:    "/index.ts",
+				},
+			},
+			htmls: 3,
+		},
+		"minimal_app": {
+			sources: []string{
+				"../test-projects/minimal/source",
+			},
+			scripts:     nil,
+			typeScripts: nil,
+			htmls:       1,
+		},
+	}
+	for name, sources := range testCases {
+		t.Run(name, func(t *testing.T) {
+			b := NewBuilder(sources, "", false)
+			if name == "minimal_app" {
+				b.HTMLExtension("htm")
+			}
+			if assert.NoError(t, b.collectFiles()) {
+				assert.Equal(t, testResults[name].sources, b.sources)
+				assert.Equal(t, testResults[name].scripts, b.scripts)
+				assert.Equal(t, testResults[name].typeScripts, b.typeScripts)
+				assert.Equal(t, testResults[name].htmls, len(b.htmls))
+			}
+		})
 	}
 }
